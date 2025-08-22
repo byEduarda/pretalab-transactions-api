089076c4b41e945396fb8e7acd96d0c5f9c15427
byEduarda <albuquerque.zxl@gmail.com>
2025-08-22 19:02:24 -0300

    create models / move data

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-import app from "./index";
+import app from "./app";
 
 const PORT = parseInt(process.env.PORT || "3000", 10);
 
--- tests/integration/transaction.spec.ts
+++ tests/integration/transaction.spec.ts
@@ -1,5 +1,5 @@
 import request from "supertest";
-import app from "../../src/index";
+import app from "../../src/app";
 import { transactions } from "../../src/models/data";
 
 describe("Testes de Integração — Transações", () => {
