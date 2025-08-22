f91d6d2f8778c6dca82dc9b270d8011730d300a4
byEduarda <albuquerque.zxl@gmail.com>
2025-08-22 19:09:35 -0300

    Update transaction.spec.ts

--- tests/integration/transaction.spec.ts
+++ tests/integration/transaction.spec.ts
@@ -1,5 +1,5 @@
 import request from "supertest";
-import app from "../../src/app";
+import app from "../../src";
 import { transactions } from "../../src/models/data";
 
 describe("Testes de Integração — Transações", () => {
