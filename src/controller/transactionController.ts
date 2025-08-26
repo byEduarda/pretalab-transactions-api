187757c6504853654dcb2a72a11e57b510b386d8
byEduarda <albuquerque.zxl@gmail.com>
2025-08-26 20:37:10 -0300

    Update transactionController.ts

--- src/controller/transactionController.ts
+++ src/controller/transactionController.ts
@@ -1,20 +1,21 @@
 import { Request, Response } from "express";
-import * as transactionService from "../services/transactionService";
-import { Transaction } from "../models/transactionModel";
-import { v4 as uuidv4 } from "uuid";
+import { getAllTransactions, getTransactionById, createTransaction } from "../services/transactionService";
+
+export const TransactionById = (req: Request, res: Response) => {
+  const transaction = getTransactionById(req.params.id);
+  if (!transaction) return res.status(404).json({ message: "Transação não encontrada" });
+  res.status(200).json({ transaction });
+};
 
 export const getTransactions = (_req: Request, res: Response) => {
-  res.json(transactionService.getAllTransactions());
+  res.status(200).json({ getTransactions });
 };
 
-export const getTransaction = (req: Request, res: Response) => {
-  const transaction = transactionService.getTransactionById(req.params.id);
-  if (!transaction) return res.status(404).json({ message: "Transação não encontrada." });
-  res.json({ transaction} );
-};
-
-export const addTransaction = (req: Request, res: Response) => {
-  const newTransaction: Transaction = { id: uuidv4(), ...req.body };
-  const saved = transactionService.createTransaction(newTransaction);
-  res.status(201).json(saved);
+export const postTransaction = (req: Request, res: Response) => {
+  try {
+    const transaction = createTransaction(req.body);
+    res.status(201).json({ transaction });
+  } catch (err: any) {
+    res.status(400).json({ message: err.message });
+  }
 };
