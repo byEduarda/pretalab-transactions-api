46026d14c6f8b52effc78e2c91ce262025c231fb
byEduarda <albuquerque.zxl@gmail.com>
2025-08-26 20:44:44 -0300

    Update purchaseModel.ts

--- src/models/purchaseModel.ts
+++ src/models/purchaseModel.ts
@@ -1,7 +1,7 @@
 export interface PurchaseItem {
   productId: string;
-  quantity: number;
   name: string;
+  quantity: number;
   price: number;
 }
 
@@ -12,4 +12,13 @@ export interface Purchase {
   items: PurchaseItem[];
 }
 
-export const purchases: Purchase[] = [];
+export const purchases: Purchase[] = [
+  {
+    id: "5eeb7f10-7ca4-4432-815d-0dc74021c91d",
+    date: new Date().toISOString(),
+    total: 7500,
+    items: [
+      { productId: "1", name: "Notebook Gamer Pro", quantity: 1, price: 7500 },
+    ],
+  },
+];
