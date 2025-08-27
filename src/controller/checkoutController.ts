159786badaee6ec2e37bc42749919cf5153bcfe2
byEduarda <albuquerque.zxl@gmail.com>
2025-08-27 20:25:35 -0300

    Update checkoutController.ts

--- src/controller/checkoutController.ts
+++ src/controller/checkoutController.ts
@@ -2,11 +2,11 @@ import { Request, Response } from "express";
 import * as checkoutService from "../services/checkoutService";
 
 export const checkout = (req: Request, res: Response) => {
-  if (!req.body.items || !Array.isArray(req.body.items)) {
-    return res.status(400).json({ message: "Items inválidos ou não enviados" });
-  }
-
   try {
+    if (!req.body || !Array.isArray(req.body.items)) {
+      return res.status(400).json({ message: "Items inválidos ou não enviados" });
+    }
+
     const newPurchase = checkoutService.processCheckout(req.body);
     res.status(201).json(newPurchase);
   } catch (err: any) {
