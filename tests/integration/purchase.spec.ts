4e1cc2e9b919e350f24bb6fc1557be396d776cd6
byEduarda <albuquerque.zxl@gmail.com>
2025-08-27 13:34:38 -0300

    Update purchase.spec.ts

--- tests/integration/purchase.spec.ts
+++ tests/integration/purchase.spec.ts
@@ -2,8 +2,8 @@ import request from "supertest";
 import app from "../../src/app";
 
 describe("Integration: Purchases / Checkout", () => {
-  it("GET /api/purchases deve retornar todas as compras", async () => {
-    const res = await request(app).get("/api/purchases");
+  it("GET /purchases deve retornar todas as compras", async () => {
+    const res = await request(app).get("/purchases");
     expect(res.status).toBe(200);
     expect(res.body).toEqual(
       expect.arrayContaining([
@@ -16,8 +16,8 @@ describe("Integration: Purchases / Checkout", () => {
     );
   });
 
-  it("GET /api/purchases/:id deve retornar compra específica", async () => {
-    const res = await request(app).get("/api/purchases/1");
+  it("GET /purchases/:id deve retornar compra específica", async () => {
+    const res = await request(app).get("/purchases/1");
     expect(res.status).toBe(200);
     expect(res.body).toMatchObject({
       id: "1",
@@ -26,13 +26,13 @@ describe("Integration: Purchases / Checkout", () => {
     });
   });
 
-  it("GET /api/purchases/:id deve retornar 404 para compra inexistente", async () => {
-    const res = await request(app).get("/api/purchases/999");
+  it("GET /purchases/:id deve retornar 404 para compra inexistente", async () => {
+    const res = await request(app).get("/purchases/999");
     expect(res.status).toBe(404);
     expect(res.body).toMatchObject({ message: "Compra não encontrada" });
   });
 
-  it("POST /api/checkout deve criar nova compra", async () => {
+  it("POST /checkout deve criar nova compra", async () => {
     const newPurchase = {
       total: 8200,
       items: [
@@ -40,19 +40,19 @@ describe("Integration: Purchases / Checkout", () => {
         { productId: "2", quantity: 1, name: "Mouse Gamer", price: 700 }
       ]
     };
-    const res = await request(app).post("/api/checkout").send(newPurchase);
+    const res = await request(app).post("/checkout").send(newPurchase);
     expect(res.status).toBe(201);
     expect(res.body).toMatchObject(newPurchase);
     expect(res.body).toHaveProperty("id");
     expect(res.body).toHaveProperty("date");
   });
 
-  it("POST /api/checkout deve retornar 400 se total > 20000", async () => {
+  it("POST /checkout deve retornar 400 se total > 20000", async () => {
     const newPurchase = {
       total: 25000,
       items: [{ productId: "1", quantity: 5, name: "Notebook Gamer Pro", price: 5000 }]
     };
-    const res = await request(app).post("/api/checkout").send(newPurchase);
+    const res = await request(app).post("/checkout").send(newPurchase);
     expect(res.status).toBe(400);
     expect(res.body).toMatchObject({ message: "O valor total da compra excede o limite de R$20.000." });
   });
