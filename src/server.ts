5ab82ffade67fa81f814ab109cf6fbad1adb4ed9
byEduarda <albuquerque.zxl@gmail.com>
2025-08-28 20:23:35 -0300

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,7 @@ import { connectToMongo } from "./database/connectToMongo";
 
 dotenv.config();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000; 
 
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
