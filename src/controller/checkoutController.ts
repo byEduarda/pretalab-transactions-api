9a11ba60e8ba84739fe45ab2a9cf1290f10fbbbb
byEduarda <albuquerque.zxl@gmail.com>
2025-08-28 13:07:44 -0300

    Corrige rota checkout

--- src/controller/checkoutController.ts
+++ src/controller/checkoutController.ts
@@ -3,13 +3,12 @@ import * as checkoutService from "../services/checkoutService";
 
 export const checkout = (req: Request, res: Response) => {
   try {
-    if (!req.body || !Array.isArray(req.body.items)) {
-      return res.status(400).json({ message: "Items inválidos ou não enviados" });
-    }
+    const { items } = req.body;
 
-    const newPurchase = checkoutService.processCheckout(req.body);
-    res.status(201).json(newPurchase);
+    const newPurchase = checkoutService.processCheckout(items);
+
+    return res.status(201).json(newPurchase);
   } catch (err: any) {
-    res.status(400).json({ message: err.message });
+    return res.status(400).json({ message: err.message });
   }
 };
--- tests/integration/checkout.spec.ts
+++ tests/integration/checkout.spec.ts
@@ -2,35 +2,39 @@ import request from "supertest";
 import app from "../../src/app";
 
 describe("Integration: Checkout", () => {
-  it("POST /checkout deve criar nova compra", async () => {
+
+  it("POST /checkout deve criar nova compra com sucesso", async () => {
     const newPurchase = {
       items: [
-        { productId: "1", quantity: 1, name: "Notebook Gamer Pro", price: 7500 },
-        { productId: "2", quantity: 1, name: "Mouse Gamer", price: 700 },
+        { productId: "1", quantity: 1, price: 7500 },
+        { productId: "2", quantity: 2, price: 350 },
       ],
     };
 
     const res = await request(app).post("/checkout").send(newPurchase);
+
     expect(res.status).toBe(201);
     expect(res.body).toMatchObject({
-      total: 8200,
       items: newPurchase.items,
+      total: 8200, 
     });
     expect(res.body).toHaveProperty("id");
     expect(res.body).toHaveProperty("date");
   });
 
-  it("POST /checkout deve retornar 400 se total > 20000", async () => {
+  it("POST /checkout deve retornar 400 se o total exceder 20000", async () => {
     const newPurchase = {
       items: [
-        { productId: "1", quantity: 5, name: "Notebook Gamer Pro", price: 5000 },
+        { productId: "1", quantity: 3, price: 7500 }, 
       ],
     };
 
     const res = await request(app).post("/checkout").send(newPurchase);
+
     expect(res.status).toBe(400);
     expect(res.body).toMatchObject({
       message: "O valor total da compra excede o limite de R$20.000.",
     });
   });
+
 });
