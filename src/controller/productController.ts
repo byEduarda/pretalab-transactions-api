dd48cea81dabd66dc9661d63938f5402e2812a10
byEduarda <albuquerque.zxl@gmail.com>
2025-08-28 22:08:51 -0300

    Update productController.ts

--- src/controller/productController.ts
+++ src/controller/productController.ts
@@ -1,12 +1,25 @@
 import { Request, Response } from "express";
 import * as productService from "../services/productService";
 
-export const getAllProducts = (req: Request, res: Response) => {
-  res.status(200).json(productService.getAllProducts());
+export const getAllProducts = async (req: Request, res: Response) => {
+  try {
+    const products = await productService.getAllProducts();
+    res.status(200).json(products);
+  } catch (error) {
+    console.error("Erro:", error);
+    res.status(500).json({ message: "Erro ao buscar produtos" });
+  }
 };
 
-export const getProductById = (req: Request, res: Response) => {
-  const product = productService.getProductById((req.params.id));
-  if (!product) return res.status(404).json({ message: "Produto não encontrado" });
-  res.status(200).json(product);
+export const getProductById = async (req: Request, res: Response) => {
+  try {
+    const product = await productService.getProductById(req.params.id);
+    if (!product) {
+      return res.status(404).json({ message: "Produto não encontrado" });
+    }
+    res.status(200).json(product);
+  } catch (error) {
+    console.error("Erro:", error);
+    res.status(500).json({ message: "Erro ao buscar produto" });
+  }
 };
