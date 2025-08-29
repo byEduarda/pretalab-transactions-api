a9d2b006191aa2f8b780b0b5db1aa6f9e539de80
byEduarda <albuquerque.zxl@gmail.com>
2025-08-28 22:08:58 -0300

    Update product.spec.ts

--- tests/unit/product.spec.ts
+++ tests/unit/product.spec.ts
@@ -2,13 +2,18 @@ import { getAllProducts, getProductById } from "../../src/services/productServic
 import { products } from "../../src/models/productModel";
 
 describe("Product Service Unit Tests", () => {
-  it("should return all products", () => {
-    const all = getAllProducts();
+  it("should return all products", async () => {
+    const all = await getAllProducts();
     expect(all).toMatchObject(products);
   });
 
-  it("should return a product by id", () => {
-    const prod = getProductById("1");
+  it("should return a product by id", async () => {
+    const prod = await getProductById("1");
     expect(prod).toMatchObject({ id: "1", name: "Notebook Gamer Pro", price: 7500 });
+  });
+
+  it("should return null for non-existing product", async () => {
+    const prod = await getProductById("999");
+    expect(prod).toBeNull();
   });
 });
