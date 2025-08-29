7c95cd933889f4428a0978816dac2ab812db755b
byEduarda <albuquerque.zxl@gmail.com>
2025-08-29 13:55:14 -0300

    products

--- src/controller/transactionController.ts
+++ src/controller/transactionController.ts
@@ -3,8 +3,18 @@ import * as transactionService from "../services/transactionService";
 import { Transaction } from "../models/transactionModel";
 import { v4 as uuidv4 } from "uuid";
 
-export const getTransactions = (_req: Request, res: Response) => {
-  res.json(transactionService.getAllTransactions());
+export const getTransactions = (req: Request, res: Response) => {
+  const filters = {
+    type: req.query.type as "income" | "expense" | undefined,
+    category: req.query.category as string | undefined,
+    startDate: req.query.startDate as string | undefined,
+    endDate: req.query.endDate as string | undefined,
+    minAmount: req.query.minAmount ? Number(req.query.minAmount) : undefined,
+    maxAmount: req.query.maxAmount ? Number(req.query.maxAmount) : undefined,
+  };
+
+  const transactions = transactionService.getAllTransactions(filters);
+  res.json(transactions);
 };
 
 export const getTransaction = (req: Request, res: Response) => {
